a4f0ad8bb955d765471ca9f4496a1665061e878e
Dmitry <dvoskoboev@gmail.com>
2021-06-12 19:22:17 +0300

    News page: added responsiveness from 1920px to 320px; added toggle cards fn.

---
+++ pages/news/news.js
@@ -0,0 +1,42 @@
+'use strict'
+
+toggleGalleryCards()
+
+function toggleGalleryCards() {
+  const
+    menuItemAll = document.querySelector('.news__menu-item--all'),
+    menuItemExterior = document.querySelector('.news__menu-item--exterior'),
+    menuItemInterior = document.querySelector('.news__menu-item--interior')
+
+  const
+    galleryItemsAll = document.querySelectorAll('.news__card'),
+    galleryItemsExterior = document.querySelectorAll(`
+      .news__card--second, 
+      .news__card--fourth, 
+      .news__card--seventh
+      `),
+    galleryItemsInterior = document.querySelectorAll(`
+      .news__card--first, 
+      .news__card--third,
+      .news__card--fifth, 
+      .news__card--sixth, 
+      .news__card--eighth, 
+      .news__card--nineth
+     `)
+
+  const toggleCards = (menuItem, galleryItems, allGallery) => {
+    menuItem.onclick = () => {
+      allGallery.forEach(item => {
+        item.style.display = 'none'
+      })
+
+      galleryItems.forEach(item => {
+        item.style.display = 'block'
+      })
+    }
+  }
+
+  toggleCards(menuItemAll, galleryItemsAll, galleryItemsAll)
+  toggleCards(menuItemExterior, galleryItemsExterior, galleryItemsAll)
+  toggleCards(menuItemInterior, galleryItemsInterior, galleryItemsAll)
+}
